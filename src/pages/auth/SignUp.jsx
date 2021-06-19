43113ff0d5977048a79649c1be6eb5f2ffc88af7
Abderrahmane <abdrr97@gmail.com>
2021-06-19 23:38:13 +0100

    adding default user profile image

--- src/pages/auth/SignUp.jsx
+++ src/pages/auth/SignUp.jsx
@@ -30,6 +30,8 @@ const SignUp = () => {
               bio: '',
               address: '',
               birthDate: '',
+              photoURL:
+                'https://firebasestorage.googleapis.com/v0/b/react-dev-book.appspot.com/o/default%2Fdefault.jpg?alt=media&token=beb80150-a8da-46f0-908f-8cbfb7ad2f4c',
               uid: user.uid,
               lastLoggedIn: timestamp(),
             })
